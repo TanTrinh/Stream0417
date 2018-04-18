f4f1fe71027ab517f043d871a43d558da5685159
Tân <trinhhoangtan96@gmail.com>
2018-04-18 13:21:19 +0700

    done connect send

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
 const openStream = require('./openStream');
 
-openStream();
+const $ = require('jquery');
+
+//openStream();
+
+const Peer = require('simple-peer');
+
+const p = new Peer({ initiator: location.hash === '#1', trickle: false});
+
+p.on('signal', token => {
+  $('#txtMySignal').val(JSON.stringify(token));
+});
+
+p.on('connect', () => console.log('connected'));
+
+$('#btnConnect').click(() => {
+  const friendSignal = JSON.parse($('#txtFriendSignal').val());
+  p.signal(friendSignal);
+})
 
 console.log('Hello World');
